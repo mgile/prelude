bddeafaeddc9d5179f181575c21073c95a3f1c1f
Michael Gile <mgile@mac.com>
2013-03-05 08:24:08 -0700

    Updated configuration

--- init.el
+++ init.el
@@ -6,6 +6,11 @@
 ;; URL: http://batsov.com/prelude
 ;; Version: 1.0.0
 ;; Keywords: convenience
+;;
+;; Modified Feb-2013 
+;; Copyright (c) 2013 Michael Gile
+;;
+
 
 ;; This file is not part of GNU Emacs.
 
@@ -32,6 +37,11 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+
+(global-linum-mode 1)
+
+(require 'package)
+(package-initialize)
 
 (message "Prelude is powering up... Be patient, Master %s!" (getenv "USER"))
 
@@ -78,6 +88,10 @@ Emacs load path."
 
 (require 'dash)
 
+;; add Prelude's directories to Emacs's `load-path'
+(add-to-list 'load-path prelude-modules-dir)
+(add-to-list 'load-path prelude-vendor-dir)
+
 ;; the core stuff
 (require 'prelude-packages)
 (require 'prelude-ui)
@@ -90,11 +104,7 @@ Emacs load path."
 (when (eq system-type 'darwin)
   (require 'prelude-osx))
 
-;; the modules
-(require 'prelude-programming)
-
-(when (file-exists-p prelude-modules-file)
-  (load prelude-modules-file))
+(setq-default default-directory "~/dev")
 
 ;; config changes made through the customize UI will be store here
 (setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
@@ -102,6 +112,164 @@ Emacs load path."
 ;; load the personal settings (this includes `custom-file')
 (when (file-exists-p prelude-personal-dir)
   (mapc 'load (directory-files prelude-personal-dir 't "^[^#].*el$")))
+
+(require 'jabber)
+(setq jabber-account-list
+      '(("mgile@symplified.com"
+         (:network-server . "talk.google.com")
+         (:connection-type . ssl))))
+
+(blink-cursor-mode 1)
+
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                      global-semanticdb-minor-mode
+                                      global-semantic-idle-summary-mode
+                                      global-semantic-decoration-mode
+                                      global-semantic-highlight-func-mode
+                                      global-semantic-stickyfunc-mode
+                                      global-semantic-mru-bookmark-mode))
+(setq semantic-load-turn-everything-on t)
+(semantic-mode 1)
+
+(require 'semantic)
+(require 'semantic/wisent)
+(require 'semantic/wisent/java-tags)
+(require 'semantic/ia)
+(require 'semantic/senator)
+(require 'semantic/sb)
+;(global-ede-mode 1)                    ;; enable project management
+(require 'semantic/bovine/gcc)                ;; use gcc headers
+(require 'semantic/java)               ;; and java too
+
+;; Malabar
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/malabar/lisp"))
+(require 'malabar-mode)
+(setq malabar-groovy-lib-dir "/Users/mgile/.emacs.d/vendor/malabar/lib")
+(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+(setq malabar-extra-source-locations '("/Users/mgile/dev/wayin/wayinserver/wayin_backend/src/main/java"))
+
+;(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/jdee/lisp"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/elib"))
+;(setq jde-web-browser "chrome")
+
+;; Use the full Java 1.5 grammer to parse Java
+;(autoload 'wisent-java-default-setup "wisent" "Hook run to setup Semantic in 'java-mode'." nil nil)
+
+;(setq jde-auto-parse-enable nil)
+;(setq jde-enable-senator nil)
+;(load "jde-autoload")
+
+;; load jde-testng
+;(require 'jde-testng)
+
+
+;; jdibug
+(setq jdibug-connect-hosts (quote ("localhost:8000"))
+      jdibug-run-jvm-args (quote ("-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y"))
+      jdibug-use-jde-source-paths nil
+      jdibug-source-paths
+      (list
+       "/Users/mgile/dev/wayin/wayinserver/wayin_backend/src/main/java"
+       "/usr/lib/jvm/java-6-sun/src"
+       )
+)
+(setq jdibug-refresh-delay 0.01)
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/jdibug"))
+(require 'jdibug)
+
+(defun my-jdibug-mode-hook ()
+  ;; IDEA style debug short cuts
+  (define-key c-mode-base-map [ (M-f8) ] 'jdibug-step-into)
+  (define-key c-mode-base-map [ (f8) ] 'jdibug-step-over)
+  (define-key c-mode-base-map [ (f9) ] 'jdibug-step-out)
+  (define-key c-mode-base-map [ (M-f7) ] 'jdibug-resume)
+  (define-key c-mode-base-map [ (M-f5) ] 'jdibug-resume-all)
+  (define-key c-mode-base-map [ (f10) ] 'jdibug-toggle-breakpoint)
+  (global-set-key "\C-\M-b" 'jdibug-connect)
+  (global-set-key "\C-\M-d" 'jdibug-disconnect)
+)
+(add-hook 'c-mode-common-hook 'my-jdibug-mode-hook)
+(add-hook 'jdibug-connected-hook 'jdibug-resume-all)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Make Emacs understand Ant's and Maven's output
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'compile)
+(setq compilation-error-regexp-alist
+  (append (list
+           ;; works for jikes
+           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):\\([0-9]+\\):[0-9]+:[0-9]+:" 1 2 3)
+           ;; works for javac
+           '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):" 1 2)
+           ;; works for maven 2.x
+           '("^\\(.*\\):\\[\\([0-9]*\\),\\([0-9]*\\)\\]" 1 2 3)
+           ;; works for maven 3.x
+           '("^\\(\\[ERROR\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
+           '("^\\(\\[WARNING\\] \\)?\\(/[^:]+\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 2 3 4)
+           )
+          compilation-error-regexp-alist))
+
+(require 'twittering-mode)
+(add-hook 'twittering-mode-hook
+           (lambda ()
+             (mapc (lambda (pair)
+                     (let ((key (car pair))
+                           (func (cdr pair)))
+                       (define-key twittering-mode-map
+                         (read-kbd-macro key) func)))
+                   '(("F" . twittering-friends-timeline)
+                     ("R" . twittering-replies-timeline)
+                     ("U" . twittering-user-timeline)
+                     ("W" . twittering-update-status-interactive)))))
+
+(setq twittering-icon-mode t)                ; Show icons
+(setq twittering-timer-interval 300)         ; Update your timeline each 300 seconds (5 minutes)
+(setq twittering-url-show-status nil)        ; Keeps the echo area from showing all the http processes
+
+;;; SmartTabs
+(require 'smarttabs)
+
+(setq-default tab-width 4) ; or any other preferred value
+    (setq cua-auto-tabify-rectangles nil)
+    (defadvice align (around smart-tabs activate)
+      (let ((indent-tabs-mode nil)) ad-do-it))
+    (defadvice align-regexp (around smart-tabs activate)
+      (let ((indent-tabs-mode nil)) ad-do-it))
+    (defadvice indent-relative (around smart-tabs activate)
+      (let ((indent-tabs-mode nil)) ad-do-it))
+    (defadvice indent-according-to-mode (around smart-tabs activate)
+      (let ((indent-tabs-mode indent-tabs-mode))
+        (if (memq indent-line-function
+                  '(indent-relative
+                    indent-relative-maybe))
+            (setq indent-tabs-mode nil))
+        ad-do-it))
+    (defmacro smart-tabs-advice (function offset)
+      `(progn
+         (defvaralias ',offset 'tab-width)
+         (defadvice ,function (around smart-tabs activate)
+           (cond
+            (indent-tabs-mode
+             (save-excursion
+               (beginning-of-line)
+               (while (looking-at "\t*\\( +\\)\t+")
+                 (replace-match "" nil nil nil 1)))
+             (setq tab-width tab-width)
+             (let ((tab-width fill-column)
+                   (,offset fill-column)
+                   (wstart (window-start)))
+               (unwind-protect
+                   (progn ad-do-it)
+                 (set-window-start (selected-window) wstart))))
+            (t
+             ad-do-it)))))
+    (smart-tabs-advice c-indent-line c-basic-offset)
+    (smart-tabs-advice c-indent-region c-basic-offset)
+
+
+;;; nXhtml
+(load "/Users/mgile/.emacs.d/vendor/nxhtml/autostart.el")
 
 (message "Prelude is ready to do thy bidding, Master %s!" (getenv "USER"))
 
